5efdf78337734c7019de60dc918e5e4f5e7d242f
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2025-06-28 12:41:30 +0100

    Week 8 Task 2

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,5 +1,4 @@
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     connection = None
@@ -21,7 +20,7 @@ def create_database():
             
             print("Database 'alx_book_store' created successfully!")
             
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error connecting to MySQL: {e}")
         
     finally:
